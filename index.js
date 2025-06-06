d031082002da46659503318a2352ca069b73f3e7
김인화 <radiantbeing99@gmail.com>
2025-06-06 21:14:12 +0900

    v0.0.2
    캡처 이미지 저장 경로의 기본값이 모듈 설치 경로로 지정되는 문제를 해결함.
    
    일부 변수명과 안내문을 상황에 적합한 표현으로 변경함.

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const __dirname = path.dirname(__filename);
 
 function captureRobot() {
     let displayId = 0;
-    let totalPages = 0;
+    let totalCaptures = 0;
     let captureIntervalMs = 500;
-    let turnPageKey = "right";
+    let autoPressKey = "right";
     let outputPath = path.join(__dirname, "output");
 
     async function listDisplays() {
@@ -23,16 +23,15 @@ function captureRobot() {
 
     function defineConfig(config) {
         displayId = config.displayId ?? displayId;
-        totalPages = config.totalPages ?? totalPages;
+        totalCaptures = config.totalCaptures ?? totalCaptures;
         captureIntervalMs = config.captureIntervalMs ?? captureIntervalMs;
-        turnPageKey = config.turnPageKey ?? turnPageKey;
+        autoPressKey = config.autoPressKey ?? autoPressKey;
         outputPath = config.outputPath ?? outputPath;
     }
 
     async function initializeDirectory(directoryPath) {
 
-// 캡처된 이미지를 저장할 출력 디렉터리를 초기화합니다. 출력 디렉터리를 생성합니다. 출력 디렉터리가 이미 생성되어 있으면 포함된 내용물을
-// 모두 삭제합니다.
+// 캡처된 이미지를 저장할 출력 디렉터리를 초기화합니다.
 
         await fs.promises.mkdir(directoryPath, {recursive: true});
 
@@ -46,8 +45,8 @@ function captureRobot() {
         });
     }
 
-    function turnPage(turnPageKey) {
-        robot.keyTap(turnPageKey);
+    function pressKey(key) {
+        robot.keyTap(key);
     }
 
     async function saveImage(filePath, buffer) {
@@ -74,27 +73,30 @@ function captureRobot() {
             await onPrepareComplete();
         }
 
-        let page = 1;
-        while (page <= totalPages) {
+        let captureNo = 1;
+        while (captureNo <= totalCaptures) {
             const imageBuffer = await screenshot({
                 format: "jpg",
                 screen: displayId
             });
-            await saveImage(path.join(outputPath, `${page}.jpg`), imageBuffer);
-            if (page < totalPages) {
-                if (validKeys.has(turnPageKey)) {
+            await saveImage(
+                path.join(outputPath, `${captureNo}.jpg`),
+                imageBuffer
+            );
+            if (captureNo < totalCaptures) {
+                if (validKeys.has(autoPressKey)) {
 
-// turnPageKey 매개변수로 전달된 키를 robotjs가 입력합니다. robotjs가 입력할 수 있는 키 목록 중 일부만을 허용하며
+// autoPressKey 매개변수로 전달된 키를 robotjs가 입력합니다. robotjs가 입력할 수 있는 키 목록 중 일부만을 허용하며
 // 매개변수로 전달된 키가 유효하지 않은 경우 아무 작업도 수행하지 않습니다.
 
-                    turnPage(turnPageKey);
+                    pressKey(autoPressKey);
                 }
                 await sleep(captureIntervalMs);
             }
             if (typeof onEachCaptureComplete === "function") {
-                await onEachCaptureComplete(page);
+                await onEachCaptureComplete(captureNo);
             }
-            page += 1;
+            captureNo += 1;
         }
 
         if (typeof onAllCaptureComplete === "function") {
@@ -121,30 +123,31 @@ async function captureRobotCli() {
         loop: false,
         message: "디스플레이 선택"
     });
-    const totalPages = Number.parseInt(await input({
+    const totalCaptures = Number.parseInt(await input({
         default: "1",
-        message: "총 페이지 수"
+        message: "캡처 매수"
     }));
     const captureIntervalMs = Number.parseInt(await input({
         default: 500,
         message: "캡처 간격 (ms)"
     }));
-    const turnPageKey = await select({
-        choices: ["right", "left", "space", "none"],
+    const autoPressKey = await select({
+        choices: ["right", "left", "space", "선택 안 함"],
         loop: false,
-        message: "페이지 넘김 키"
+        message: "캡처 직후 자동 입력 키"
     });
     const outputPath = await input({
-        default: path.join(__dirname, "output"),
-        message: "이미지 저장 경로"
+        default: path.resolve("./output"),
+        message: "캡처 이미지 저장 경로 (경로 내 파일은 모두 삭제됨)"
     });
     cr.defineConfig({
+        autoPressKey,
         captureIntervalMs,
         displayId,
         outputPath,
-        totalPages,
-        turnPageKey
+        totalCaptures
     });
+
     cr.startCapture({
         onAllCaptureComplete: () => console.log("✅ 모든 캡처가 완료되었습니다."),
         onEachCaptureComplete: (page) => console.log(`[${page}] 캡처 완료`),
