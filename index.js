091b0b31b5175c7a55655eaa6722f67e4c75fe29
김인화 <radiantbeing99@gmail.com>
2025-06-07 21:34:28 +0900

    CLI 모듈 분리 및 메인 객체 생성자 수정
    CLI 프로세스에 관여하는 함수를 cli.js로 분리하여 메인 객체의 constructor와 명확히 역할을 구분함.
    
    메인 객체 생성 함수의 이름을 captureRobot에서 createCaptureRobot으로 변경함. new 키워드와 함께
    사용되는 생성자 함수로 오해할 소지를 낮추기 위함임.

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-#!/usr/bin/env node
 /*jslint node*/
 import fs from "node:fs";
 import path from "node:path";
 import {fileURLToPath} from "node:url";
-import {input, select} from "@inquirer/prompts";
-import screenshot from "screenshot-desktop";
 import robot from "robotjs";
+import screenshot from "screenshot-desktop";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-function captureRobot() {
+function createCaptureRobot() {
+
+// 클로저 기반의 캡처 자동화 로봇 객체를 생성합니다.
+
     let displayId = 0;
     let totalCaptures = 0;
     let captureIntervalMs = 500;
@@ -104,61 +105,12 @@ function captureRobot() {
         }
     }
 
-    return Object.freeze({
+    return {
         defineConfig,
         listDisplays,
         sleep,
         startCapture
-    });
+    };
 }
 
-async function captureRobotCli() {
-    const cr = captureRobot();
-
-    const displayId = await select({
-        choices: (await cr.listDisplays()).map((display) => ({
-            name: display.name,
-            value: display.id
-        })),
-        loop: false,
-        message: "디스플레이 선택"
-    });
-    const totalCaptures = Number.parseInt(await input({
-        default: "1",
-        message: "캡처 매수"
-    }));
-    const captureIntervalMs = Number.parseInt(await input({
-        default: 500,
-        message: "캡처 간격 (ms)"
-    }));
-    const autoPressKey = await select({
-        choices: ["right", "left", "space", "선택 안 함"],
-        loop: false,
-        message: "캡처 직후 자동 입력 키"
-    });
-    const outputPath = await input({
-        default: path.resolve("./output"),
-        message: "캡처 이미지 저장 경로 (경로 내 파일은 모두 삭제됨)"
-    });
-    cr.defineConfig({
-        autoPressKey,
-        captureIntervalMs,
-        displayId,
-        outputPath,
-        totalCaptures
-    });
-
-    cr.startCapture({
-        onAllCaptureComplete: () => console.log("✅ 모든 캡처가 완료되었습니다."),
-        onEachCaptureComplete: (page) => console.log(`[${page}] 캡처 완료`),
-        onPrepareComplete: async function () {
-            const startDelayMs = 5000;
-            console.log(`⏳ ${startDelayMs / 1000}초 뒤 캡처가 시작됩니다.`);
-            await cr.sleep(startDelayMs);
-        }
-    });
-}
-
-export default Object.freeze(captureRobot);
-
-captureRobotCli();
+export default Object.freeze(createCaptureRobot);
