ef344e9adf15fa5ee0c13670d78d6d54bed223cd
김인화 <radiantbeing99@gmail.com>
2025-06-14 16:55:42 +0900

    ESLint 규칙을 `@radiantbeing/eslint-config` 패키지로 모듈화
    여러 프로젝트에서 공통된 코드 규칙을 갖게 하기 위해 자주 사용되는 린팅 규칙을 독립적인
    패키지로 모듈화함. `eslint.config.js` 파일에 열거된 규칙을 정리하고, 추가된 패키지
    를 확장하도록 변경함.

--- eslint.config.js
+++ eslint.config.js
@@ -2,68 +2,25 @@
 import {defineConfig} from "eslint/config";
 import globals from "globals";
 import js from "@eslint/js";
+import radiant from "@radiantbeing/eslint-config";
 import stylistic from "@stylistic/eslint-plugin";
 
 export default Object.freeze(defineConfig([
     {
-        extends: ["js/recommended"],
+        extends: [
+            "js/recommended",
+            radiant.configs.js
+        ],
         files: ["**/*.{js,mjs,cjs}"],
-        plugins: {js},
-        rules: {
-            "curly": ["error", "all"],
-            "no-unused-vars": [
-                "error",
-                {caughtErrorsIgnorePattern: "^ignore"}
-            ],
-            "sort-imports": "error",
-            "sort-keys": ["error", "asc"]
-        }
+        plugins: {js}
     },
     {
-        extends: ["@stylistic/recommended"],
+        extends: [
+            "@stylistic/recommended",
+            radiant.configs.stylistic
+        ],
         files: ["**/*.{js,mjs,cjs}"],
-        plugins: {
-            "@stylistic": stylistic
-        },
-        rules: {
-            "@stylistic/arrow-parens": ["error", "always"],
-            "@stylistic/block-spacing": "off",
-            "@stylistic/brace-style": [
-                "error",
-                "1tbs",
-                {allowSingleLine: true}
-            ],
-            "@stylistic/comma-dangle": ["error", "never"],
-            "@stylistic/indent": ["error", 4, {
-                ignoreComments: true,
-                ignoredNodes: [
-                    "ConditionalExpression",
-                    "TSUnionType",
-                    "TSIntersectionType",
-                    "TSTypeParameterInstantiation",
-                    "FunctionExpression > .params[decorators.length > 0]",
-                    "FunctionExpression > "
-                    + ".params > "
-                    + ":matches(Decorator, :not(:first-child))"
-                ],
-                offsetTernaryExpressions: false
-            }],
-            "@stylistic/max-len": ["error", {
-                code: 80,
-                comments: 80,
-                ignoreRegExpLiterals: true,
-                ignoreStrings: true,
-                ignoreTemplateLiterals: true,
-                ignoreUrls: true
-            }],
-            "@stylistic/max-statements-per-line": "off",
-            "@stylistic/object-curly-spacing": ["error", "never"],
-            "@stylistic/operator-linebreak": ["error", "before"],
-            "@stylistic/padded-blocks": "off",
-            "@stylistic/quotes": ["error", "double"],
-            "@stylistic/semi": ["error", "always"],
-            "@stylistic/spaced-comment": "off"
-        }
+        plugins: {"@stylistic": stylistic}
     },
     {
         files: ["**/*.{js,mjs,cjs}"],
