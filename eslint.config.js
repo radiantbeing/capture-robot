975b0516b2a6a67e553940b934bb50f16d3e32dc
김인화 <radiantbeing99@gmail.com>
2025-06-18 22:35:14 +0900

    ESLint 구성 단순화 및 의존성 업데이트

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,12 @@
 /*jslint node */
 import {defineConfig} from "eslint/config";
 import globals from "globals";
-import js from "@eslint/js";
 import radiant from "@radiantbeing/eslint-config";
-import stylistic from "@stylistic/eslint-plugin";
 
 export default Object.freeze(defineConfig([
     {
-        extends: [
-            "js/recommended",
-            radiant.configs.js
-        ],
-        files: ["**/*.{js,mjs,cjs}"],
-        plugins: {js}
-    },
-    {
-        extends: [
-            "@stylistic/recommended",
-            radiant.configs.stylistic
-        ],
-        files: ["**/*.{js,mjs,cjs}"],
-        plugins: {"@stylistic": stylistic}
+        extends: [radiant.configs.recommended],
+        files: ["**/*.{js,mjs,cjs}"]
     },
     {
         files: ["**/*.{js,mjs,cjs}"],
